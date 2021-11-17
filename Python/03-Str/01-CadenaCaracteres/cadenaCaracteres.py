d0b13f7f9e45ed87f331712648f1dacbfaf42920
Iván Coello Cortés <ivancoellocortes@gmail.com>
2021-11-17 09:55:05 +0100

    .

--- Python/03-Str/01-CadenaCaracteres/cadenaCaracteres.py
+++ Python/03-Str/01-CadenaCaracteres/cadenaCaracteres.py
@@ -3,18 +3,12 @@
 # La primera letra de cada palabra. Por ejemplo, si recibe Universal Serial Bus debe devolver USB.
 def iniciales(cadena):
     lista=cadena.split(" ")
-    iniciales=""
-    for palabra in lista:
-        iniciales += palabra[0]
-    return iniciales
+    return "".join(palabra[0] for palabra in lista)
 
 # Dicha cadena con la primera letra de cada palabra en mayúsculas. Por ejemplo, si recibe república argentina debe devolver República Argentina.
-def capitalizacion(cadena):
+def capitalizar(cadena):
     lista = cadena.split(" ")
-    capitalizadas = ""
-    for palabra in lista:
-        capitalizadas += palabra.capitalize() + " "
-    return capitalizadas
+    return "".join(palabra.capitalize() + " " for palabra in lista)
 
 # Las palabras que comiencen con la letra A. Por ejemplo, si recibe Antes de ayer debe devolver Antes ayer.
 def palabras_comienzan_a(cadena):
